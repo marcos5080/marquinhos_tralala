24e15583f66bd9a61ff55144e66124ed8b779b11
marcos5080 <araujomarcos5080@gmail.com>
2026-01-29 22:12:22 -0300

    Add files via upload

---
+++ topo.php
@@ -0,0 +1,46 @@
+
+<div id="caixaTopo">
+    <div class="container">
+
+        <div class="row">
+        
+            <div class="col-md-12">
+                   <ul class="nav justify-content-center">
+                      
+                        <div class="nav-item">
+                            <a class="nav-link active" href="home.php">home</a>
+                        </div>
+                        <div class="nav-item">
+                            <a class="nav-link" href="cadastro.php">Cadastro</a>
+                        </div>
+                        <div class="nav-item">
+                            <a class="nav-link" href="informacoes.php">informacoes</a>
+                        </div>
+                        
+                    </ul>
+
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
