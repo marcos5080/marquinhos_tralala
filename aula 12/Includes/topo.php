7b9a65ecba189073b0f7801f04e4c518c6ec0c8e
marcos5080 <araujomarcos5080@gmail.com>
2026-03-10 22:13:21 -0300

    Add files via upload

--- aula 12/Includes/topo.php
+++ aula 12/Includes/topo.php
@@ -13,8 +13,19 @@
                         <div class="nav-item">
                             <a class="nav-link" href="../View/home2.php">Home carroa</a>
                         </div>
+
                         <div class="nav-item">
                             <a class="nav-link" href="../View/cadastrar2.php">Carros</a>
+                        </div>
+                         <div class="nav-item dropdown">
+                            <button class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="" role="button" aria-expanded="false">Dropdown</button>
+                            <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../View/cidade.php">Cidade</a></li>
+                            <li><a class="dropdown-item" href="../View/estado.php">Estado</a></li>
+                            <li><a class="dropdown-item" href="../View/pais.php">País</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="../View/enviar_email.php">Email</a></li>
+                            </ul>
                         </div>
                         <div class="nav-item">
                             <a class="nav-link" onclick=" return confirm('tem certeza que deseja sair?')" href="../Services/sair_action.php">Sair</a>
